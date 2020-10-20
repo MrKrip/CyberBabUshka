6db0d10aae516d8099e83db1dcbab4ef0c0297fd
MrKrip <45828333+MrKrip@users.noreply.github.com>
2020-10-20 15:42:52 +0300

    Some changes
    +Entity Freamwork
    +Change webconfig

---
+++ CyberBabushka/Models/Repository/EFDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CyberBabushka.Models.Repository
+{
+    public class EFDbContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
