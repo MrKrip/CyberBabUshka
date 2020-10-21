ea23f4917157ece92efd59812f3ad05842f5ac86
Vlad gribovskiy <45690086+Trafi25@users.noreply.github.com>
2020-10-21 13:27:31 +0300

    Нужно пофиксить мою слепоту

--- CyberBabushka/App_Start/RouteConfig.cs
+++ CyberBabushka/App_Start/RouteConfig.cs
@@ -16,8 +16,13 @@ namespace CyberBabushka
             routes.MapPageRoute(null, "list/{page}", "~/Pages/Listing.aspx");
             routes.MapPageRoute(null, "", "~/Pages/Listing.aspx");
             routes.MapPageRoute(null, "list", "~/Pages/Listing.aspx");
+
             routes.MapPageRoute("cart", "cart", "~/Pages/CartView.aspx");
             routes.MapPageRoute("checkout", "checkout", "~/Pages/Checkout.aspx");
+
+            
+            routes.MapPageRoute("admin_orders", "admin/orders", "~/Pages/Admin/Orders.aspx");
+            routes.MapPageRoute("admin_products", "admin/games", "~/Pages/Admin/Games.aspx");
         }
     }
 }
