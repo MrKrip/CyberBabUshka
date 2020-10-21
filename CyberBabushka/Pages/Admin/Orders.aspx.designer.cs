ff971740ba7c27cf82ee3afb709ba496af8585d4
Vlad gribovskiy <45690086+Trafi25@users.noreply.github.com>
2020-10-21 14:18:05 +0300

    God is here !!!

--- CyberBabushka/Pages/Admin/Orders.aspx.designer.cs
+++ CyberBabushka/Pages/Admin/Orders.aspx.designer.cs
@@ -13,5 +13,23 @@ namespace CyberBabushka.Pages.Admin
 
     public partial class Orders
     {
+
+        /// <summary>
+        /// Repeater1 элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater Repeater1;
+
+        /// <summary>
+        /// showDispatched элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox showDispatched;
     }
 }
