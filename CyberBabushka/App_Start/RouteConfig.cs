6ec5371c16017940d2b5b7d21c710a6006f4753b
MrKrip <45828333+MrKrip@users.noreply.github.com>
2020-10-21 15:20:33 +0300

    Пока что конец,но скоро вернемся

--- CyberBabushka/App_Start/RouteConfig.cs
+++ CyberBabushka/App_Start/RouteConfig.cs
@@ -23,6 +23,7 @@ namespace CyberBabushka
             
             routes.MapPageRoute("admin_orders", "admin/orders", "~/Pages/Admin/Orders.aspx");
             routes.MapPageRoute("admin_products", "admin/products", "~/Pages/Admin/Products.aspx");
+            routes.MapPageRoute("Login", "Login", "~/Pages/Login.aspx");
         }
     }
 }
