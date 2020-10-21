17981c7ca0ae4551c3852cbe5ca4476f92d4fc77
Vlad gribovskiy <45690086+Trafi25@users.noreply.github.com>
2020-10-21 10:54:25 +0300

    покатит на мвп но можно лучше

--- CyberBabushka/Controls/VInput .cs
+++ CyberBabushka/Controls/VInput .cs
@@ -1,40 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
 namespace CyberBabushka.Controls
 {
-    [DefaultProperty("Text")]
-    [ToolboxData("<{0}:VInput runat=server></{0}:VInput>")]
     public class VInput : WebControl
     {
-        [Bindable(true)]
-        [Category("Appearance")]
-        [DefaultValue("")]
-        [Localizable(true)]
-        public string Text
-        {
-            get
-            {
-                String s = (String)ViewState["Text"];
-                return ((s == null) ? String.Empty : s);
-            }
+        private string nspace = "CyberBabushka.Models";
+        private string model = "Order";
 
-            set
-            {
-                ViewState["Text"] = value;
-            }
+        public string Namespace
+        {
+            get { return nspace; }
+            set { nspace = value; }
         }
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public string Property { get; set; }
 
         protected override void RenderContents(HtmlTextWriter output)
         {
-            output.Write(Text);
+
+            output.AddAttribute(HtmlTextWriterAttribute.Id, Property.ToLower());
+            output.AddAttribute(HtmlTextWriterAttribute.Name, Property.ToLower());
+
+            Type modelType = Type.GetType(string.Format("{0}.{1}", Namespace, Model));
+            PropertyInfo propInfo = modelType.GetProperty(Property);
+            var attr = propInfo.GetCustomAttribute<RequiredAttribute>(false);
+            if (attr != null)
+            {
+                output.AddAttribute("data-val", "true");
+                output.AddAttribute("data-val-required", attr.ErrorMessage);
+            }
+            output.RenderBeginTag("input");
+            output.RenderEndTag();
+        }
+
+        public override void RenderBeginTag(HtmlTextWriter writer)
+        {
+        }
+        public override void RenderEndTag(HtmlTextWriter writer)
+        {
         }
     }
 }
--- CyberBabushka/Pages/Checkout.aspx
+++ CyberBabushka/Pages/Checkout.aspx
@@ -1,5 +1,5 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Checkout.aspx.cs" Inherits="CyberBabushka.Pages.Checkout" MasterPageFile="~/Pages/Store.Master" %>
-
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Checkout.aspx.cs" Inherits="CyberBabushka.Pages.Checkout"
+    MasterPageFile="~/Pages/Store.Master" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="bodyContent" runat="server">
     <div id="content">
@@ -18,25 +18,25 @@
             <h3>Заказчик</h3>
             <div>
                 <label for="name">Имя:</label>
-                <input id="name" name="name" data-val="true" data-val-required="Введите имя"/>
+                <SX:VInput Property="Name" runat="server" />
             </div>
 
             <h3>Адрес доставки</h3>
             <div>
                 <label for="line1">Адрес 1:</label>
-                <input id="line1" name="line1" />
+                <SX:VInput Property="Line1" runat="server" />
             </div>
             <div>
                 <label for="line2">Адрес 2:</label>
-                <input id="line2" name="line2" />
+                <SX:VInput Property="Line2" runat="server" />
             </div>
             <div>
                 <label for="line3">Адрес 3:</label>
-                <input id="line3" name="line3" />
+                <SX:VInput Property="Line3" runat="server" />
             </div>
             <div>
                 <label for="city">Город:</label>
-                <input id="city" name="city" />
+                <SX:VInput Property="City" runat="server" />
             </div>
 
             <h3>Детали заказа</h3>
